6430a946c612b6ab45f55a551c3ead1d54798efc
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-05-07 22:44:41 +0700

    Фиксация начальных изменений

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import Costs from "./components/Costs/Costs";
 
 function App() {
+
+  const costs = [
+    {
+      date: new Date(2023, 2, 12),
+      description: 'Холодильник',
+      amount: 999.99
+    },
+    {
+      date: new Date(2022, 4, 29),
+      description: 'Анаконда',
+      amount: 1245
+    },
+    {
+      date: new Date(2022, 12, 1),
+      description: 'Пиколини',
+      amount: 12.13
+    }
+  ]
+
   return (
     <div>
-      <h1>
-        Начнем же!!!
-      </h1>
+      <h1>Начнем же!!!</h1>
+        <Costs costs={costs}/>
     </div>
   );
 }
